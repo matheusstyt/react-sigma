7baeb57bd183599bad269cda8844870f3d3cf0b0
tiago19deoliveira <tiago18deoliveira@gmail.com>
2022-10-13 16:42:44 -0400

    commit do prg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <h1>AMAZON REACT</h1>
+      <p> Primeiro parágrafo...</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
